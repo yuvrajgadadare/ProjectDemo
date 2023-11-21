e6655eeeb3d9294413ca07b1795878ab9bfafd96
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2023-11-21 14:51:33 +0530

    Initial commit

---
+++ ProjectDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDemo
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a = 10, b = 20, c;
+            c = a + b;
+            Console.WriteLine("Add="+c);
+        }
+        static void Main()
+        {
+            Program p = new Program();
+            p.Add();
+            Console.ReadLine();
+        }
+    }
+}
